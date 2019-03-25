c20ec4a51a4682d4aff6035757f3705a8fb5a05f
Luci Peterson <lucijpeterson@gmail.com>
2019-03-25 13:22:02 -0700

    Created object and for loop to print to console
    Created object to store information about a book and used a for/in loop to print it to the console.

--- practice_js_object_literals/beginning_project_files/1_object.js
+++ practice_js_object_literals/beginning_project_files/1_object.js
@@ -11,12 +11,22 @@ If your program is stuck in an infinite loop, you can break out of the program b
 // 2. Add a title property to the book object and assign it a string value
 // 3. Add an author property to the book object and assign it a string value
 // 4. Add a publish_year to the book object and assign it a number to represent the year the book was published
+const book = {
+    title: "Dragon Teeth",
+    author: "Michael Crighton",
+    publish_year: 2009
+}
 
+console.log(book);
 
 /* 5. Use a for-in loop to print the book object to the console so the final output looks something like this:
 title: Harry Potter and the Sorcerer's Stone
 author: J.K. Rowling
 publish_year: 1997
 */
+
+for (i in book) {
+    console.log(i + ": " + book[i])
+}
 
 // 6. Run your code by typing node 1_object.js in the console below
